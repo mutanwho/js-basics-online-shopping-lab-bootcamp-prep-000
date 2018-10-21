aeaceb3e8f549b94597472670ace5081e3ffc2f5
Michelle M <mutanwho@gmail.com>
2018-10-21 21:37:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,5 +51,12 @@ function removeFromCart(item) {
   
 
 function placeOrder(cardNumber) {
- 
+  if (cardNumber === undefined) {
+    return("Sorry, we don't have a credit card on file for you.");
+  } else { 
+    var message = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+   cart = []; 
+    return message;
+  }
+}
 
