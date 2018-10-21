74a9890ff2dc8ad59c918fd70188b3dac3f4819e
Michelle M <mutanwho@gmail.com>
2018-10-21 21:21:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,9 +54,9 @@ function placeOrder(cardNumber) {
   if(cardNumber === undefined){
     return "Sorry, we don't have a credit card on file for you."
   } else {
-  var message = 
+  var message = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   cart.splice(0, cart.length)
-  return `Your total cost is $${amt}, which will be charged to the card ${cardNumber}.`;
+  return message;
   }
 }
   
