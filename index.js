e0e7c3a74eb557214c1bea4be940e5ad350310ac
Michelle M <mutanwho@gmail.com>
2018-10-21 21:03:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,15 +40,14 @@ function total() {
 
 function removeFromCart(item) {
   for(var i = 0; i < cart.length; i++){
-   if(item === cart[i].itemName) {
+   if(cart[i].itemName === item) {
      cart.splice(i, 1);
   return cart;
    }
-  } else(item != cart[item]){
-    return "That item is not in your cart."
+  } 
+    return "That item is not in your cart.";
   }
   
-}
 
 function placeOrder(cardNumber) {
   // write your code here
